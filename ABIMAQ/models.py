16e0a74428313e507fa1d66c562c7795a201734e
jve00 <jv666776@gmail.com>
2025-08-14 21:03:36 -0300

    criando o app ABIMAQ

---
+++ ABIMAQ/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class PesquisaABIMAQ(models.model):
+     STATUS_CHOICES = (
+        ('pendente', 'Pendente'),
+        ('carregando', 'Carregando'),
+        ('sucesso', 'Sucesso'),
+        ('erro', 'Erro'), 
+    )
+     
+     arquivo_upload = models.FileField(upload_to='uploads/')
+     status = models.CharField(choices=STATUS_CHOICES,default='pendente')
+     caminho_resultado = models.CharField(max_length=250,null=True, blank=True)
+     data_criacao = models.DateTimeField(auto_now_add=True)
